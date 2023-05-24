095371d5fea69dc44ed48933712a6e2a8681cef8
Irakli Iremashvili <iraklifordev@gmail.com>
2023-05-24 22:00:07 +0400

    fix main.py arguments

--- main.py
+++ main.py
@@ -8,20 +8,26 @@ from utils.key_pair import create_key_pair
 from utils.security_group import create_security_group, add_security_group_rules
 
 def main():
-    parser = argparse.ArgumentParser(description="AWS VPC and IGW CLI")
-    parser.add_argument("vpc_name", help="Name of the VPC to be created")
-    parser.add_argument("vpc_id", help="ID of the VPC")
-    parser.add_argument("subnet_id", help="ID of the Subnet")
+    parser = argparse.ArgumentParser(description="AWS VPC and EC2 CLI")
+    subparsers = parser.add_subparsers(dest="command")
+
+    create_vpc_parser = subparsers.add_parser("create-vpc", help="Create a VPC")
+    create_vpc_parser.add_argument("vpc_name", help="Name of the VPC to be created")
+
+    launch_ec2_parser = subparsers.add_parser("launch-ec2", help="Launch an EC2 instance")
+    launch_ec2_parser.add_argument("vpc_id", help="ID of the VPC")
+    launch_ec2_parser.add_argument("subnet_id", help="ID of the Subnet")
+
     args = parser.parse_args()
 
     client = init_client()
 
-    if args.vpc_name:
+    if args.command == "create-vpc":
         vpc_id = create_vpc(client, args.vpc_name)
 
         igw_id = create_igw(client)
         attach_igw_to_vpc(client, vpc_id, igw_id)
-    elif args.vpc_id and args.subnet_id:
+    elif args.command == "launch-ec2":
         security_group_id = create_security_group(client, args.vpc_id)
         add_security_group_rules(client, security_group_id)
 
